47ac31736e6f3f9477d65cd8332fb025aef4b57c
Abu Hasan Rumi <message.rumi@gmail.com>
2020-11-20 20:56:48 +0600

    achievement area - responsive done

--- src/Components/Home/AchievementArea/AchievementArea.js
+++ src/Components/Home/AchievementArea/AchievementArea.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AchievementArea.css'
-import { faLaughBeam } from '@fortawesome/free-solid-svg-icons'
+import { faAward, faBrain, faLaughBeam, faRocket } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const AchievementArea = () => {
@@ -22,38 +22,46 @@ const AchievementArea = () => {
                     <div className="col-md-6 py-4">
                         <div className="row achievement-cards ">
                             <div className="col-md-6 achievement-card-wrapper py-3">
-                                <div className="card single-achievement achievement1 d-flex justify-content-center flex-row align-items-center p-3">
-                                    <div className="achievementImg pr-4"><FontAwesomeIcon icon={faLaughBeam} /></div>
-                                    <div className="card-body p-0 d-inline-block">
-                                        <h5 className="card-title achievementTitle m-0">700+</h5>
-                                        <p className="card-text achievementText">Happy Clients</p>
+                                <div className="card single-achievement achievement1 px-3 py-5 py-md-4">
+                                    <div className="card-details">
+                                        <div className="achievementImg pr-4"><FontAwesomeIcon icon={faLaughBeam} /></div>
+                                        <div className="card-body p-0 d-inline-block">
+                                            <h5 className="card-title achievementTitle m-0">700+</h5>
+                                            <p className="card-text achievementText">Happy Clients</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-md-6 achievement-card-wrapper py-3">
-                                <div className="card single-achievement achievement2 d-flex justify-content-center flex-row align-items-center p-3">
-                                    <div className="achievementImg pr-4"><FontAwesomeIcon icon={faLaughBeam} /></div>
-                                    <div className="card-body p-0 d-inline-block">
-                                        <h5 className="card-title achievementTitle m-0">140+</h5>
-                                        <p className="card-text achievementText">Projects Completed</p>
+                                <div className="card single-achievement achievement2 px-3 py-5 py-md-4">
+                                    <div className="card-details">
+                                        <div className="achievementImg pr-4"><FontAwesomeIcon icon={faAward} /></div>
+                                        <div className="card-body p-0 d-inline-block">
+                                            <h5 className="card-title achievementTitle m-0">140+</h5>
+                                            <p className="card-text achievementText">Projects Completed</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-md-6 achievement-card-wrapper py-3">
-                                <div className="card single-achievement achievement2 d-flex justify-content-center flex-row align-items-center p-3">
-                                    <div className="achievementImg pr-4"><FontAwesomeIcon icon={faLaughBeam} /></div>
-                                    <div className="card-body p-0 d-inline-block">
-                                        <h5 className="card-title achievementTitle m-0">25+</h5>
-                                        <p className="card-text achievementText">Crazy Minds</p>
+                                <div className="card single-achievement achievement2 px-3 py-5 py-md-4">
+                                    <div className="card-details">
+                                        <div className="achievementImg pr-4"><FontAwesomeIcon icon={faBrain} /></div>
+                                        <div className="card-body p-0 d-inline-block">
+                                            <h5 className="card-title achievementTitle m-0">25+</h5>
+                                            <p className="card-text achievementText">Crazy Minds</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-md-6 achievement-card-wrapper py-3">
-                                <div className="card single-achievement achievement1 d-flex justify-content-center flex-row align-items-center p-3">
-                                    <div className="achievementImg pr-4"><FontAwesomeIcon icon={faLaughBeam} /></div>
-                                    <div className="card-body p-0 d-inline-block">
-                                        <h5 className="card-title achievementTitle m-0">75+</h5>
-                                        <p className="card-text achievementText">Running Projects</p>
+                                <div className="card single-achievement achievement1 px-3 py-5 py-md-4">
+                                    <div className="card-details">
+                                        <div className="achievementImg pr-4"><FontAwesomeIcon icon={faRocket} /></div>
+                                        <div className="card-body p-0 d-inline-block">
+                                            <h5 className="card-title achievementTitle m-0">75+</h5>
+                                            <p className="card-text achievementText">Running Projects</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -61,7 +69,7 @@ const AchievementArea = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
---
+++ src/Components/Home/PackageArea/PackageArea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PackageArea = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PackageArea;
